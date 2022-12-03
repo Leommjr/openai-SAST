f007adad92770933b1670a26c215112d9a27d64a
Leonardo <leommjr09@gmail.com>
2022-12-03 12:38:10 -0300

    Create simple script. Works fine in small projects

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# openia-SAST
-SAST tool using GPT-3
+# openai-SAST
+
+openai-SAST is a static analysis tool that uses the OpenAI API to detect vulnerabilities in code files. It currently supports Python, Go, JavaScript, and PHP code files, but can be easily extended to support other languages.
+
+## Installation
+
+Before using openai-SAST, you need to install its dependencies. The required dependencies are specified in the `requirements.txt` file. You can install these dependencies using `pip`:
+
+```
+pip install -r requirements.txt
+```
+
+Alternatively, you can install the dependencies using `conda`:
+
+```
+conda install --file requirements.txt
+```
+
+## Usage
+
+To use openai-SAST, you need an API key for the OpenAI API. You can provide the API key as the second command line argument when running the `openai-SAST.py` script:
+
+```
+python openai-SAST.py <directory> <api_key>
+```
+
+Alternatively, you can set the `OPENAI_API_KEY` environment variable with your API key and run the script without providing the key as a command line argument:
+
+```
+export OPENAI_API_KEY=<api_key>
+python openai-SAST.py <directory>
+```
+
+The `<directory>` argument specifies the directory that contains the code files to be analyzed. If no directory is specified, the current directory (`.`) will be used.
+
+## Output
+
+openai-SAST will recursively read all of the code files in the specified directory and its subdirectories and use the OpenAI API to detect vulnerabilities in the code. The detected vulnerabilities will be printed to the console in the following format:
+
+```
+Vulnerabilities in <filename>:
+- <vulnerability> on line <line_number>
+- <vulnerability> on line <line_number>
+- ...
+```
+
+## Limitations
+
+Currently, openai-SAST only detects vulnerabilities and does not provide any information on how to fix them. Additionally, it is only as accurate as the OpenAI model it uses, so false positives and false negatives may occur. It is recommended to manually review the detected vulnerabilities to verify their accuracy. 
---
+++ sast.py
@@ -0,0 +1,51 @@
+import openai
+import os
+import sys
+
+# Set the API key to the value of the second command line argument,
+# the OPENAI_API_KEY environment variable, or a default value if
+# neither the command line argument nor the environment variable
+# is provided.
+openai_api_key = sys.argv[2] if len(sys.argv) > 2 else os.environ.get("OPENAI_API_KEY", "default_api_key")
+
+# Set the API key for the openai module.
+openai.api_key = openai_api_key
+
+# The directory that contains the code files to be analyzed.
+# Set the directory to the first command line argument or a default
+# value of "./" if no command line argument is provided.
+directory = sys.argv[1] if len(sys.argv) > 1 else "./"
+
+
+def check_vulnerabilities(filename):
+    # Check if the file has a code file extension.
+    if os.path.splitext(filename)[1] in (".py", ".go", ".js", ".php"):
+        # Read the code from the file.
+        with open(filename, encoding='utf-8') as f:
+            code = f.read()
+        PROMPT = f"Please provide only a list of vulnerabilities and the line in which it manifests itself in the following code:\n{code}"
+        MAX_TOKENS = 4000 - len(PROMPT)
+        # Use GPT-3 to detect vulnerabilities in the code.
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=PROMPT,
+            temperature=0.5,
+            max_tokens=MAX_TOKENS,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+
+        # Extract the list of vulnerabilities from the GPT-3 response.
+        vulnerabilities = response["choices"][0]["text"].strip().split("\n")
+
+        # Print the list of vulnerabilities detected in the code.
+        print(f"Vulnerabilities in {filename}:")
+        for vulnerability in vulnerabilities:
+            print(f"- {vulnerability}")
+
+
+# Recursively read all of the files in the specified directory and its subdirectories.
+for root, dirs, files in os.walk(directory):
+    for filename in files:
+        check_vulnerabilities(os.path.join(root, filename))
